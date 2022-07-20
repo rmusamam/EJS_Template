7bbef5f549fe26a52dfe1446df83947692eae18b
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-07-20 16:28:34 +0500

    template is deployed

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app= express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res)=>{
+    var today=new Date()
+    var currentDay= today.getDay();
+    var day=""
+    if(currentDay == 0 || currentDay==6){
+        day= "weekend"
+    }
+    else{
+        day="weekday"
+    }
+    res.render('list',{kindOfDay:day})
+})
